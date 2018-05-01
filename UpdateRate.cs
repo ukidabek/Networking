47f79c14e94c9f0666cdd97342687c2e09fadc87
Ukidabek <ukidabek@gmail.com>
2018-05-01 12:34:55 +0200

    - Input handling update.

---
+++ UpdateRate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+[SerializeField]
+public class UpdateRate
+{
+    [SerializeField] private int _frequency = 60;
+    public float Interval { get { return 1f / _frequency; } }
+}

76751393001fab87bb4e33d42daa32938c2b26e8
Ukidabek <ukidabek@gmail.com>
2018-05-23 19:46:31 +0200

    - Documentation update.

--- NetworkUtility.cs
+++ NetworkUtility.cs
@@ -6,10 +6,18 @@ using UnityEngine.Networking.Types;
 
 namespace Networking
 {
+    /// <summary>
+    /// Network utilities class
+    /// </summary>
     public static class NetworkUtility
     {
         private static readonly char[] IP_ADRES_SEPARATORS = { ':' };
 
+        /// <summary>
+        /// Returns IP address form string.
+        /// </summary>
+        /// <param name="ipAdressString">String contains IP</param>
+        /// <returns></returns>
         public static string GetIPAdres(string ipAdressString)
         {
             string[] ipAdresParts = ipAdressString.Split(IP_ADRES_SEPARATORS);
@@ -17,11 +25,24 @@ namespace Networking
             return ipAdresParts[ipAdresIndex];
         }
 
+        /// <summary>
+        /// Converts network error byte to network error. 
+        /// </summary>
+        /// <param name="error">Error byte</param>
+        /// <returns>Network error</returns>
         public static NetworkError GetNetworkError(byte error)
         {
             return (NetworkError)error;
         }
 
+        /// <summary>
+        /// Converts byte array to object
+        /// </summary>
+        /// <typeparam name="T">Type of object</typeparam>
+        /// <param name="array">Byte array.</param>
+        /// <param name="start">Start index</param>
+        /// <param name="length">Length of object array</param>
+        /// <returns>Object</returns>
         public static T ConvertBytesToObject<T>(byte[] array, int start = 0, int length = 0)
         {
             BinaryFormatter binaryFormatter = new BinaryFormatter();
@@ -32,6 +53,11 @@ namespace Networking
             return objectFormBytes;
         }
 
+        /// <summary>
+        /// Converts object to byte array
+        /// </summary>
+        /// <param name="objectToConvert">Object to convert</param>
+        /// <returns>Bytes array</returns>
         public static byte[] ConvertObjectToBytes(object objectToConvert)
         {
             BinaryFormatter binaryFormatter = new BinaryFormatter();
--- UpdateRate.cs
+++ UpdateRate.cs
@@ -4,9 +4,22 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-[SerializeField]
-public class UpdateRate
+namespace Networking
 {
-    [SerializeField] private int _frequency = 60;
-    public float Interval { get { return 1f / _frequency; } }
+    /// <summary>
+    /// Helper class defining rate of updates pro network updators.  
+    /// </summary>
+    [SerializeField]
+    public class UpdateRate
+    {
+        /// <summary>
+        /// Frequency of objects
+        /// </summary>
+        [SerializeField] private int _frequency = 60;
+
+        /// <summary>
+        /// Interval of updates in seckends. 
+        /// </summary>
+        public float Interval { get { return 1f / _frequency; } }
+    }
 }
